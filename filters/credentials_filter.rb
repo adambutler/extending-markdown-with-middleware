748454128212235e1133daf08a183d778156a03c
Adam Butler <adam@lab.io>
2017-02-14 09:20:27 +0000

    gsub -> gsub!

--- filters/credentials_filter.rb
+++ filters/credentials_filter.rb
@@ -2,7 +2,7 @@
 class CredentialsFilter < Banzai::Filter
   def call(input)
     # e.g. Either "Hello Adam" or "Hello world"
-    input.gsub("$USERNAME", current_user ? current_user.first_name : "World")
+    input.gsub!("$USERNAME", current_user ? current_user.first_name : "World")
 
     # e.g. Either "ABC123"  or "EXAMPLE_API_KEY"
     input.gsub("$API_KEY", current_user ? current_user.api_key : "EXAMPLE_API_KEY")
